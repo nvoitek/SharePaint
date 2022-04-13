52f66ef09930285792a10fa2094c1dfc432e5cd8
nvoitek <nvoitek@gmail.com>
2022-04-14 00:08:25 +0200

    Small models improvement

--- api/SharePaint.Models/Shape.cs
+++ api/SharePaint.Models/Shape.cs
@@ -8,7 +8,7 @@ namespace SharePaint.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string Id { get; set; }
 
         public ShapeType ShapeType { get; set; }
 
